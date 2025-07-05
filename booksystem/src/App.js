84b643fb55d781889ba2429499ca599ce87bd514
Tatsuya Nakajima <coptis923@gmail.com>
2025-07-05 13:58:26 +0900

    create header

--- booksystem/src/App.js
+++ booksystem/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import Header from './header/header.js';
 
 function App() {
   return (
-      <h1>
-        Test
-      </h1>
+    <Header></Header>
   );
 }
 
---
+++ booksystem/src/header/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <h1 className="site-title">📚 書籍管理システム</h1>
+      </div>
+
+      <div className="header-center">
+        <input
+          type="text"
+          placeholder="ISBN または 書名で検索"
+          className="search-input"
+        />
+        <button className="search-button">検索</button>
+      </div>
+
+      <div className="header-right">
+        <a href="/books" className="nav-link">一覧表示</a>
+        <a href="/register" className="nav-link">登録</a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
